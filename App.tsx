a459be0c83057f0bb941710dd879df116025fa90
Antonio Dambujo <pdambuja@gmail.com>
2022-05-25 13:10:10 +0200

    First commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <FontAwesomeIcon icon={faCoffee} size={50} />
+
       <StatusBar style="auto" />
     </View>
   );
